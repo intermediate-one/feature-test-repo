4132b0992aa8bc02cce685ca1309a88bf3f39afc
jericho31 <a@a.a>
2024-01-17 16:51:46 +0900

    feat : 클립보드 기능

--- app/src/main/java/com/example/testproject1/ClipboardActivity.kt
+++ app/src/main/java/com/example/testproject1/ClipboardActivity.kt
@@ -1,8 +1,11 @@
 package com.example.testproject1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.testproject1.databinding.ActivityClipboardBinding
+
 
 class ClipboardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityClipboardBinding
@@ -11,5 +14,11 @@ class ClipboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityClipboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btn3.setOnClickListener {
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("label", binding.et3.text)
+            clipboard.setPrimaryClip(clip)
+        }
     }
 }
